f204d5c7a48e44b3c541d7e52b236cd02cf7d723
Alexandr Petrovich <apalion97@gmail.com>
2020-10-08 09:31:08 +0300

    commit 8.10

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,10 @@
 import './config/jqueryLoad';
 import 'bootstrap/dist/js/bootstrap.min';
+import 'slick-carousel/slick/slick.min';
 
+$('.slider-container').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false
+});
