1e3785798541023caa289aedf0911e9c54691d39
Alexandr Petrovich <apalion97@gmail.com>
2020-10-12 16:30:04 +0300

    commit 12.10

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,5 +6,23 @@ $('.slider-container').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    dots: false
+    dots: false,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
